f94276fdc5592809f6d9772ea9fee863b674ffe4
phuc20057371 <foxfessor@gmail.com>
2023-10-20 14:14:22 +0700

    first commit

---
+++ src/config/db/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+async function connect(){
+    try{
+        await mongoose.connect('mongodb://127.0.0.1:27017/education_dev', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        //    useCreateIndex: true
+        })
+        console.log('Database: Connected')
+    }catch(e){
+        console.log('Database: Error')
+        console.log(e)
+    }
+}
+
+module.exports = { connect }
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+
+const newsRouter = require('./news')
+const siteRouter = require('./site')
+const coursesRouter = require('./courses')
+const meRouter = require('./me')
+
+function routes(app) {
+  app.use('/news', newsRouter)
+  app.use('/courses', coursesRouter)
+  app.use('/me', meRouter)
+  app.use('/', siteRouter)
+}
+
+module.exports = routes
+
